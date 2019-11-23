bc53b9fd9d11af78e4e74c361bdb434e9b86781e
SHINNYCHOU <1437947307@qq.com>
2019-11-23 17:08:05 +0800

    2

---
+++ src/components/admin/InAndOut/ins.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "ins"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/admin/InAndOut/outs.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "outs"
+    }
+</script>
+
+<style scoped>
+
+</style>
